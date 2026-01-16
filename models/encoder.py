ae7d6e2fd5d28d6dba2c3a36450e6b9cabe852ec
Kacper Lesiakowski <167813699+liquidcatty@users.noreply.github.com>
2026-01-16 21:07:12 +0100

    made fully working(at least on paper encoder)

--- models/encoder.py
+++ models/encoder.py
@@ -9,8 +9,9 @@ def encode_card(card):
 
 action_map = {"fold": 0, "check": 1, "call": 2, "raise": 3}
 
-def encoder(game_history, num_players=5):
+def encoder(game_history, hole_cards_by_action):
     encoded_seq = []
+    action_counter = 0
 
     for round_info in game_history["rounds"]:
         shared_cards = round_info.get("community_card", [])
@@ -24,8 +25,15 @@ def encoder(game_history, num_players=5):
             action_type = action["action"]
             amount = action.get("amount", 0)
             stack_before = action.get("stack_before", 0)
-            step_vector = [player_idx, action_map[action_type], amount, stack_before] + cards_encoded
+
+            hole_cards = hole_cards_by_action[action_counter]
+            if hole_cards is None:
+                hole_encoded = [-1, -1]
+            else:
+                hole_encoded = [encode_card(c) for c in hole_cards]
+            step_vector = [player_idx, action_map[action_type], amount, stack_before] + cards_encoded + hole_encoded
 
 
             encoded_seq.append(step_vector)
+
     return torch.tensor(encoded_seq, dtype=torch.float32)
